4b59fb1fdc1e50f182602cb35e5dacda05945af3
hcxs <1145470390@qq.com>
2018-03-26 17:57:55 +0800

    xg

--- frontend/themes/brest/layouts/main_header.php
+++ frontend/themes/brest/layouts/main_header.php
@@ -7,7 +7,8 @@
  */
 $theme = $this->theme;
 $this->registerCss(".panel{width:580px;}.header{background:#000;}.nav_m .er_m{background:#eee;}
-.tel{text-align:right;color:gray;font-size:12px;margin-top:10px;}.qr{width:60px;height: 60px;margin-left:10px;}");
+.tel{text-align:right;color:gray;font-size:12px;margin-top:10px;position:relative}
+.qr_top_right{width:60px;height: 60px;}");
 $assets = \frontend\themes\brest\AppAsset::register($this);
 $categories = \common\models\entity\Category::find()->all();
 ?>
@@ -16,11 +17,12 @@ $categories = \common\models\entity\Category::find()->all();
     <div class="head clearfix">
         <div class="logo"><a href="/"><img src="<?= "$assets->baseUrl/images/logo.png" ?>" alt="公司名称"/></a></div>
         <div class="head_r clearfix">
-            <div class="tel">Tel：0760-23309133/0760-23309136<img class="qr" src="<?= "$assets->baseUrl/images/site_qrcode.jpg" ?>" /></div>
-            <form action="/search" method="get" class="search">
-                <input name="tag" type="text">
-                <input name="" type="submit" value="">
-            </form>
+            <div class="tel">Tel：0760-23309133/0760-23309136</div>
+            <div style="float: right;"><img class="qr_top_right" src="<?= "$assets->baseUrl/images/site_qrcode.jpg" ?>" /></div>
+<!--            <form action="/search" method="get" class="search">-->
+<!--                <input name="tag" type="text">-->
+<!--                <input name="" type="submit" value="">-->
+<!--            </form>-->
             <div class="nav_m">
                 <div class="n_icon"><?= Yii::t('app.t2', 'Nav') ?></div>
                 <ul class="nav clearfix">
@@ -30,6 +32,12 @@ $categories = \common\models\entity\Category::find()->all();
                     <li><a href="/requirement"><?= Yii::t('app.t2', 'Sells and service') ?></a></li>
                     <li><a href="/contact-us"><?= Yii::t('app.t2', 'Contact us') ?></a></li>
                     <li><a href="/news"><?= Yii::t('app.t2', 'News') ?></a></li>
+                    <li>
+                        <form action="/search" method="get" class="search">
+                            <input name="tag" type="text">
+                            <input name="" type="submit" value="">
+                        </form>
+                    </li>
                 </ul>
                 <div class="er_m">
                     <div class="hx">
