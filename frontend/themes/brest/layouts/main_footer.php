acf5d3a0b0139a9ebb3f1a6bb0914173165e9c3e
hcxs <1145470390@qq.com>
2018-04-04 16:03:49 +0800

    kf

--- frontend/themes/brest/layouts/main_footer.php
+++ frontend/themes/brest/layouts/main_footer.php
@@ -10,21 +10,13 @@ use common\tools\Setting;
 
 $theme = $this->theme;
 $this->registerCss(".link{color:#37c6c0;text-decoration:underline;}
-.qr{position: absolute;top: -90px;left: 20px;width: 100px !important;height: 100px !important;display:none;}
-.bq{height:30px;line-height:30px;}.fn_bg .f_nav li span{color:#444;}.head .search input[type=\"text\"]{padding-left:10px;}
+.bq{height:30px;line-height:30px;}
+.fn_bg .f_nav li span{color:#444;}
+.head .search input[type=\"text\"]{padding-left:10px;}
 ");
 $assets = \frontend\themes\brest\AppAsset::register($this);
 ?>
 <div class="space_hx">&nbsp;</div>
-<!--<div class="friend">-->
-<!--    <div class="b_head"><span>--><?//= Yii::t("app.t2", "Friend link") ?><!--</span></div>-->
-<!--    <div class="frd_m">-->
-<!--        <a href="">Baidu</a>-->
-<!--        <a href="">Alia</a>-->
-<!--        <a href="">Google</a>-->
-<!--        <a href="">Sina</a>-->
-<!--    </div>-->
-<!--</div>-->
 <div class="fn_bg">
     <ul class="f_nav clearfix">
         <li>
@@ -57,15 +49,12 @@ $assets = \frontend\themes\brest\AppAsset::register($this);
         }
     ?>
 </div>
-<script type="text/javascript">
-//    $("#follow").on("mouseover mouseout",function(event){
-//        if(event.type == "mouseover"){
-//            //鼠标悬浮
-//            $("#qr").show();
-//        }else if(event.type == "mouseout"){
-//            //鼠标离开
-//            $("#qr").hide();
-//
-//        }
-//    })
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?7dd9def5876f2fabb9cc31c159c91183";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
 </script>
